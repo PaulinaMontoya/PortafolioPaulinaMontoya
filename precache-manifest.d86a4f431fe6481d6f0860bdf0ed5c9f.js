0b797df741c15605a24a9cb3471afe03de274d85
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-10-17 17:46:48 -0500

    Updates

---
+++ precache-manifest.d86a4f431fe6481d6f0860bdf0ed5c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692764777203b52a0fd01a67211e2208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305b7c504817f619cce7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "205077743e1bc62837b9",
+    "url": "/static/js/2.c67c5a6f.chunk.js"
+  },
+  {
+    "revision": "305b7c504817f619cce7",
+    "url": "/static/js/main.4c45ce8f.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
