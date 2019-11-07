6e27f0c260560e2e735a9d55042af8adfabd9f72
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-11-06 19:05:16 -0600

    I Created the grid to proyects component

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,9 +8,6 @@ class Card extends Component {
         return (
           <div className="cointenerAllCards">
 
-
-              
-            {/*  
             <div className="cointenerCard">
                 <img className="imgCard" src={Img} />
                 <div className="infoProyect">
@@ -22,8 +19,56 @@ class Card extends Component {
                     </div>
                 </div>
             </div>
-            */}
 
+            <div className="cointenerCard">
+                <img className="imgCard" src={Img} />
+                <div className="infoProyect">
+                    <h3>Nombre del Proyecto</h3>
+                    <p>Descripción del proyecto bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.......</p>
+                    <div className="cointenerButtons">
+                        <button className="buttonProyect">DEMO</button>
+                        <button className="buttonProyect">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="cointenerCard">
+                <img className="imgCard" src={Img} />
+                <div className="infoProyect">
+                    <h3>Nombre del Proyecto</h3>
+                    <p>Descripción del proyecto bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.......</p>
+                    <div className="cointenerButtons">
+                        <button className="buttonProyect">DEMO</button>
+                        <button className="buttonProyect">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="cointenerCard">
+                <img className="imgCard" src={Img} />
+                <div className="infoProyect">
+                    <h3>Nombre del Proyecto</h3>
+                    <p>Descripción del proyecto bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.......</p>
+                    <div className="cointenerButtons">
+                        <button className="buttonProyect">DEMO</button>
+                        <button className="buttonProyect">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="cointenerCard">
+                <img className="imgCard" src={Img} />
+                <div className="infoProyect">
+                    <h3>Nombre del Proyecto</h3>
+                    <p>Descripción del proyecto bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.......</p>
+                    <div className="cointenerButtons">
+                        <button className="buttonProyect">DEMO</button>
+                        <button className="buttonProyect">GitHub</button>
+                    </div>
+                </div>
+            </div>
+            
+            
           </div>  
       );
     }
