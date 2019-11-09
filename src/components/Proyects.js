c555f2605e0d23293a7df9ebad95f596cc41e34c
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-11-08 18:00:12 -0600

    I changed a broken link

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -24,7 +24,7 @@ const Proyects = {
             'name': 'Burter Kawaii',
             'description': 'Aplicación para tablet pensada para un restaurante de hamburesas con la cual se pueden generar comandas que se guaradan en una base de datos y se mandan a cocina y el cocinero puede avisar cuando la orden está lista y ésta comanda desaparece de la pantalla',
             'demo': 'https://paulinamontoya.github.io/burger-kawaii/',
-            'github': 'https://github.com/PaulinaMontoya/burger-kawaiii',
+            'github': 'https://github.com/PaulinaMontoya/burger-kawaii',
             'technologies': ["React.js", "Create-React-App", "React-Router", "Vanilla-CSS3", "Fetch"]
         },
         {
