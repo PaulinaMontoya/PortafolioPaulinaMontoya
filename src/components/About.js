b6e3c1ad1934877972c457f0031f9d48533526c0
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-12-01 16:30:48 -0600

    Apostrofe

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
         <Menu />
         <div className="cointenerTextAbout">
           <img src={pau} alt="" className="photoPau" ></img>
-          <p className="titleAbout">Hello! I´m <span className="spanContact" >Paulina Montoya</span></p>
+          <p className="titleAbout">Hello! I'm <span className="spanContact" >Paulina Montoya</span></p>
           <p className="aboutMe">I studied a Bachelor Degree in Graphic Design and have experience developing brands and designing interfaces to web sites, e-commerces, landing pages and mobile apps. In the last 3 years I've specialized in web design and web development attending workshops, taking online courses and currently finishing Laboratoria's bootcamp as a Front-end Developer. I enjoy teamwork but I can also work independently. 
 <br/>
 <span className="spanContact" >I love keeping my creativity in constant exploration about new trends on design, art and new technologies.</span></p>
