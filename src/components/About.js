7d11a2c130683d6790285d881ff54f77b8e97f33
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-12-01 16:25:06 -0600

    I changed some menu styles

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,9 @@ class About extends Component {
         <div className="cointenerTextAbout">
           <img src={pau} alt="" className="photoPau" ></img>
           <p className="titleAbout">Hello! I´m <span className="spanContact" >Paulina Montoya</span></p>
-          <p className="aboutMe">I studied a Bachelor Degree in Graphic Design and have experience developing brands and designing interfaces to web sites, e-commerces, landing pages and mobile apps.
-In the last 3 years I've specilized me in web design and web development attending workshops, taking online courses and currently I'm finishing Laboratoria's bootcamp as a Front-end Developer.
-I enjoy teamwork but also can work independently. 
+          <p className="aboutMe">I studied a Bachelor Degree in Graphic Design and have experience developing brands and designing interfaces to web sites, e-commerces, landing pages and mobile apps. In the last 3 years I've specialized in web design and web development attending workshops, taking online courses and currently finishing Laboratoria's bootcamp as a Front-end Developer. I enjoy teamwork but I can also work independently. 
 <br/>
-<span className="spanContact" >I love keep my creative being in constant exploration about new trend on design, art and new technologies.</span></p>
+<span className="spanContact" >I love keeping my creativity in constant exploration about new trends on design, art and new technologies.</span></p>
           <div className="cointenerButtonsAbout">
             <Link to="/contact">
               <button className="buttonAboutContact"><a target="_blank">Contact me</a></button>
--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -4,7 +4,7 @@ const Proyects = {
             'id': 1,
             'img': 'https://i.ibb.co/FB5DqBN/burger-Kawaii-App-Talento.png',
             'name': 'Burger Kawaii',
-            'description': 'It\'s a web app to be used in ipads. It creates orders to a burger restaurant with two menus where the chefs can see the order in real time and delete them when they are ready.',
+            'description': 'It\'s a web app to be use in tablets. It creates orders to a burger restaurant with two menus where the chefs can see the orders in real time and delete them when they are ready.',
             'demo': 'https://vimeo.com/376056295',
             'github': 'https://github.com/PaulinaMontoya/burger-kawaii',
             'technologies': ["React.js", "Create-React-App", "React-Router", "CSS3", "HTML5", "Responsive Web", "Fetch"]
@@ -13,7 +13,7 @@ const Proyects = {
             'id': 2,
             'img': 'https://i.ibb.co/0KzpMNS/guanataco-App-Talento.png',
             'name': 'Guanataco',
-            'description': 'It is a social network to write posts with comments about places to eat tacos in the city of Guadalajara and other users can rate them.',
+            'description': 'It\'s a social network to write posts with comments about places to eat tacos in the city of Guadalajara and other users can rate them.',
             'demo': 'https://vimeo.com/376266190',
             'github': 'https://github.com/PaulinaMontoya/GDL003-social-network',
             'technologies': ["Vanilla-JavaScript", "CSS3", "HTML5", "Firebase-Authentication", "Cloud Firestore"]
@@ -22,7 +22,7 @@ const Proyects = {
             'id': 3,
             'img': 'https://i.ibb.co/0V2QX5y/morty-App-Talento.png',
             'name': 'Morty\'s Nightmares',
-            'description': 'It is a 2D platform game created with Unity. If you want to dowloand it the build is in my GitHub account but it only works in windows.',
+            'description': 'It\'s a 2D platform game created with Unity. If you want to dowloand it the build is in my GitHub account but it only works in windows.',
             'demo': 'https://vimeo.com/376002772',
             'github': 'https://github.com/PaulinaMontoya/GDL003-Videogame-2D-Rick-and-Morty',
             'technologies': ["Unity", "C#"]
